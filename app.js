726d39198cde2a61816d1cba7cab759588424d77
Pranav Gupta <pranav@whiz-solutions.com>
2018-05-28 10:47:36 +0530

    updates to deploy to heroku

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'false'}));
 app.use(express.static(path.join(__dirname, 'dist')));
+
 app.use('/books', express.static(path.join(__dirname, 'dist')));
 app.use('/book-create', express.static(path.join(__dirname, 'dist')));
 app.use('/book-details/:id', express.static(path.join(__dirname, 'dist')));
